c2bb6417fe00737a97caf0fbc80c3817cb2f0e6f
Igor <igor@trouble.tr>
2015-06-22 13:05:25 -0300

    Added main.cpp and data files

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <tools.h>
+#include <stdlib.h> 
+#include <list>
+
+using namespace std;
+using namespace tools;
+
+int main()
+{
+
+
+   return 0;
+}
